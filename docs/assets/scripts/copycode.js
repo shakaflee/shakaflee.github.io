e3b271c8b89f8ba025c4b6366206621927d672a2
shakaflee <62050863+shakaflee@users.noreply.github.com>
2023-07-10 23:19:53 +0800

    update style.css and copycode.html
    1. 更新导航栏为固定；
    2. 更新copycode的样式，添加了图标；
    3. 更新相应的css.

--- docs/assets/scripts/copycode.js
+++ docs/assets/scripts/copycode.js
@@ -9,17 +9,17 @@ copyCodeButtons.forEach((copyCodeButton, index) => {
     // Copy the code to the user's clipboard
     window.navigator.clipboard.writeText(code);
 
-    // Update the button text visually
-    const { innerText: originalText } = copyCodeButton;
-    copyCodeButton.innerText = 'Copied!';
+    // Update the button text and insert SVG icon
+    const svgIconCopied = `<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>`;
 
-    // (Optional) Toggle a class for styling the button
-    copyCodeButton.classList.add('copied');
+    const svgIconCopy = `<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>`;
+
+    copyCodeButton.innerHTML = `${svgIconCopied} Copied!`;
 
     // After 2 seconds, reset the button to its initial UI
     setTimeout(() => {
-      copyCodeButton.innerText = originalText;
-      copyCodeButton.classList.remove('copied');
+      copyCodeButton.innerHTML = `${svgIconCopy} Copy code`;
+      copyCodeButton.classList.remove(`${svgIconCopied} Copied`);
     }, 2000);
   });
 });
