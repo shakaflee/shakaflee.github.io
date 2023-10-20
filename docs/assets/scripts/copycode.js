22c84bd15a2bcb36edc33ae09838c23dcf7a7331
shakaflee <62050863+shakaflee@users.noreply.github.com>
2023-10-20 19:00:04 +0800

    back to my

---
+++ docs/assets/scripts/copycode.js
@@ -0,0 +1,25 @@
+// This assumes that you're using Rouge; if not, update the selector
+const codeBlocks = document.querySelectorAll('.code-header + .highlighter-rouge');
+const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+
+copyCodeButtons.forEach((copyCodeButton, index) => {
+  const code = codeBlocks[index].innerText;
+
+  copyCodeButton.addEventListener('click', () => {
+    // Copy the code to the user's clipboard
+    window.navigator.clipboard.writeText(code);
+
+    // Update the button text and insert SVG icon
+    const svgIconCopied = `<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>`;
+
+    const svgIconCopy = `<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>`;
+
+    copyCodeButton.innerHTML = `${svgIconCopied} Copied!`;
+
+    // After 2 seconds, reset the button to its initial UI
+    setTimeout(() => {
+      copyCodeButton.innerHTML = `${svgIconCopy} Copy code`;
+      copyCodeButton.classList.remove(`${svgIconCopied} Copied`);
+    }, 2000);
+  });
+});
